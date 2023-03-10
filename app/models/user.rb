24cde11297679511b36d36a8b693b103c30b9130
KatKeeeen <katken-dangdingdong-1007@ezweb.ne.jp>
2023-03-10 16:58:19 +0900

    usersバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :password,
+              format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
+    validates :nickname
+    validates :birth_day
+  end
 end
