4e6c49c7f878fbcd0857722e02bb0b1d965128bc
KatKeeeen <katken-dangdingdong-1007@ezweb.ne.jp>
2023-03-14 23:34:27 +0900

    イベント投稿機能実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,27 @@
 class EventsController < ApplicationController
+
   def index
     @journal = Journal.find(params[:journal_id])
+    @events = @journal.events
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.create(event_params)
+    if @event.save
+      redirect_to journal_events_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:date, :time, :place, :comment, :image).merge(journal_id: params[:journal_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'journals#index'
   resources :journals, only: [:index, :new, :create, :show] do
-    resources :events, only:[:index, :create]
+    resources :events, only:[:index, :new, :create]
   end
 end
