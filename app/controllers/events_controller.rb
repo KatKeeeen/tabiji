4ff2c42e587a18651e5c234709e638c1c2ccbb29
KatKeeeen <katken-dangdingdong-1007@ezweb.ne.jp>
2023-04-10 23:03:18 +0900

    コントローラーの調整

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,14 @@
 class EventsController < ApplicationController
 
+  before_action :move_to_sign_in
+  before_action :journal_setting, only: [:index, :new, :create, :edit, :update]
+  before_action :event_setting, only: [:edit, :update, :destroy]
+
   def index
-    @journal = Journal.find(params[:journal_id])
-    @events = @journal.events
+    @events = @journal.events.sort {|a, b| a[:time] <=> b[:time]}
   end
 
   def new
-    @journal = Journal.find(params[:journal_id])
     @event = Event.new
   end
 
@@ -15,34 +17,45 @@ class EventsController < ApplicationController
     if @event.save
       redirect_to journal_events_path
     else
-      @journal = Journal.find(params[:journal_id])
+
       render :new
     end
   end
 
   def edit
-    @journal = Journal.find(params[:journal_id])
-    @event = Event.find(params[:id])
   end
 
   def update
-    @event = Event.find(params[:id])
     @event.update(event_params)
     if @event.save
       redirect_to journal_events_path
     else
-      @journal = Journal.find(params[:journal_id])
+
       render :edit
     end
   end
 
   def destroy
-    event = Event.find(params[:id])
-    event.destroy
+    @event.destroy
     redirect_to journal_events_path
   end
 
+
   private
+
+  def move_to_sign_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def journal_setting
+    @journal = Journal.find(params[:journal_id])
+  end
+
+  def event_setting
+    @event = Event.find(params[:id])
+  end
 
   def event_params
     params.require(:event).permit(:date, :time, :place, :comment, :image).merge(journal_id: params[:journal_id])
